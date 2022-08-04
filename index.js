abd486b5bcc78b885f5ff0a0061390f90e757527
Gacheruian <ian.gacheru@student.moringaschool.com>
2022-08-05 02:38:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
 // Write your solution in this file!
-const employee ={
+const employee =
+{
     name: "Sam",
     streetAddress:"11 Broadway",
 }
-function updateEmployeeWithKeyAndValue(employee, key, value){
+function updateEmployeeWithKeyAndValue(employee, key, value)
+{
     const updateEmp = {...employee}
     updateEmp[key] = value;
     return updateEmp;
 
 }
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+updateEmployeeWithKeyAndValue()
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value)
+{
     employee[key] = value;
     return employee;
 }
+destructivelyUpdateEmployeeWithKeyAndValue()
+
 function deleteFromEmployeeByKey(employee, key){
     const  updateEmp = { ...employee };
     delete updateEmp [key];
     return updateEmp ;
 }
+deleteFromEmployeeByKey()
+
 function destructivelyDeleteFromEmployeeByKey(employee, key){
     delete employee[key];
     return employee;
 }
+destructivelyDeleteFromEmployeeByKey()
